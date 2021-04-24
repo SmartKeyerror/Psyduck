4489022dee0d30a9d1abdced81f6c895d99f9dfb
smartkeyerror <smartkeyerror@gmail.com>
2021-04-24 12:03:56 +0800

    :construction:

--- go-concurrency/mutex/mutex_test.go
+++ go-concurrency/mutex/mutex_test.go
@@ -1,6 +1,7 @@
 package mutex
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 )
@@ -23,4 +24,51 @@ func foo(number int, mutex sync.Locker) {
 func TestReentrantMutex(t *testing.T) {
 	var mutex sync.Mutex
 	foo(10, &mutex)
+}
+
+// 我们也可以使用 Mutex 来实现单例模式，比如配置文件的生成
+
+type Config struct {
+	Host    string
+	Port    uint16
+	Workers uint8
+}
+
+var config *Config
+
+// 传统的双重校验锁方式，不过这种写法在其它一些语言中，例如 C++ 中，会因为指令重排的原因导致创建多个实例
+// 所以其实现在很少有人会这么用，支持静态变量的语言都会利用“静态变量只会初始化一次”的特性来实现
+// 而 Go 中则提供了 sync.Once 来实现单例
+func GetConfig() *Config {
+	var mutex sync.Mutex
+
+	if config == nil {
+		mutex.Lock()
+		if config == nil {
+			config = &Config{
+				Host:    "127.0.0.1",
+				Port:    8080,
+				Workers: 4,
+			}
+		}
+		mutex.Unlock()
+	}
+	return config
+}
+
+func TestSingletonConfig(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			config := GetConfig()
+			fmt.Printf("config address : %p \n", config)
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Printf("config address : %p", GetConfig())
 }
